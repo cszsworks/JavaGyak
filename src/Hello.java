b796140ddda40fbe0fc00c7a53afcff741b9e070
Csimbók Zsolt <cszsworks@gmail.com>
2025-01-10 11:06:13 +0100

    Commit first class

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+    public static void main(String[] args){
+        boolean isAlien = false;
+        if (isAlien == true)
+            System.out.println("Hello Alien!");
+        else System.out.println("Hello World!");
+
+        int topScore = 100;
+        if(topScore >= 100) {
+            System.out.println("You got the high score!");
+        }
+
+    }
+
+
+}
